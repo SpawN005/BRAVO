426266588f08a9dbb1cd2c24ac4be8b59485cf2a
SpawN 005 <jasserbouzidi22@gmail.com>
2023-02-19 17:58:40 +0100

    add + check oeuvre detail

---
+++ src/main/java/com/example/PIDEV/FeedController.java
@@ -0,0 +1,80 @@
+package com.example.PIDEV;
+
+import entity.Oeuvre;
+import javafx.fxml.FXML;
+
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Orientation;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import service.ServiceOeuvre;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+
+public class FeedController implements Initializable {
+    @FXML
+    private BorderPane pane;
+    @FXML
+    private AnchorPane feed ;
+    @FXML
+    private ScrollPane ScrollPane;
+    AnchorPane anchorPane = null;
+    ServiceOeuvre so=new ServiceOeuvre();
+    List<Oeuvre> l=so.readAll();
+
+    @FXML
+    protected void onImageClick(Oeuvre o) {
+        FXMLLoader loader=new FXMLLoader(getClass().getResource("DetailsOeuvre.fxml"));
+        Parent root= null;
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        DetailsOeuvreController dc=loader.getController();
+        dc.SetOeuvre(o);
+        feed.getScene().setRoot(root);
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        double  y = 274.0;
+        double x =58.0;
+        for (Oeuvre o : l){
+            anchorPane = new AnchorPane();
+            System.out.println(o.getUrl());
+            Image image = new Image("file:src/main/resources/com/example/PIDEV/assets/"+o.getUrl());
+            ImageView paint= new ImageView(image);
+            Label title=new Label(o.getTitle());
+            Label owner=new Label(o.getOwner());
+            Label description=new Label(o.getDescription());
+            description.setWrapText(true);
+            description.setMaxWidth(200);
+            anchorPane.setLayoutX(x);
+            title.setLayoutY(y);
+            owner.setLayoutY(y+18);
+            description.setLayoutY(y+36);
+            anchorPane.getChildren().addAll(paint,title,owner,description);
+            anchorPane.setOnMouseClicked(mouseEvent -> onImageClick(o));
+            anchorPane.setStyle("-fx-cursor: hand;");
+            feed.getChildren().addAll(anchorPane);
+            x+=263.0;
+        }
+        ScrollPane.setStyle("-fx-background-color: transparent;");
+    }
+
+}
--- src/main/java/com/example/PIDEV/Main.java
+++ src/main/java/com/example/PIDEV/Main.java
@@ -2,19 +2,17 @@ package com.example.PIDEV;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("addOeuvre.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1125, 770);
-        stage.setTitle("Feed Page!");
+        stage.setTitle("tun'ART");
         stage.setScene(scene);
         stage.show();
     }
