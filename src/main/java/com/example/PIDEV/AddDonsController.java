546ea8ac968e37b82701dd70a1ad55e12309856a
ElMajj <aimen.majoul@gmail.com>
2023-02-21 14:59:02 +0100

    Added Dons enity and ServiceDons

---
+++ src/main/java/com/example/PIDEV/AddDonsController.java
@@ -0,0 +1,15 @@
+package com.example.PIDEV;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AddDonsController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
