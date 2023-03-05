b3cce386ca23b9076d96c8d0c431ddf4e7182e6f
SpawN 005 <jasserbouzidi22@gmail.com>
2023-03-05 21:11:12 +0100

    bug fixed

--- src/main/java/com/example/PIDEV/Main.java
+++ src/main/java/com/example/PIDEV/Main.java
@@ -3,9 +3,11 @@ import entity.CommentaireOeuvre;
 import entity.NoteOeuvre;
 import entity.Oeuvre;
 import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -16,8 +18,14 @@ import service.ServiceOeuvre;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -28,7 +36,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("addOeuvre.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1125, 770);
         stage.setTitle("tun'ART");
         scene.setFill(Color.TRANSPARENT);
@@ -43,7 +51,7 @@ public class Main extends Application {
         stage.show();
     }
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) throws IOException, InterruptedException {
         launch();
     }
     };
